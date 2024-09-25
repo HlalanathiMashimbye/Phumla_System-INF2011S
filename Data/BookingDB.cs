cf23f7e412b599e4e8f400caac6f32fad45108a7
Hlalanathi Mashimbye <hlalianoint@gmail.com>
2024-09-25 20:06:03 +0200

    Modify Database +Configure Connection

--- Data/BookingDB.cs
+++ Data/BookingDB.cs
@@ -1,12 +1,88 @@
-﻿using System;
+﻿using Phumla_System.Properties;
+using System.Data;
+using System.Data.SqlClient;
 using System.Collections.Generic;
+using System;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Phumla_System.Data
 {
-    internal class BookingDB
+    public class BookingDB
     {
+        //enum to store operations
+        public enum BookingDBoperatios
+        {
+            Add = 0,
+            Change = 1,
+            Delete = 2,
+        }
+
+        #region Variables
+        private string strConn = Settings.Default.BookingsDatabaseConnectionString;
+        protected SqlConnection SqlConnection;
+        protected DataSet DataSet;
+        protected SqlDataAdapter DataAdapter;
+        #endregion
+
+        #region Constructor
+        public BookingDB() {
+            try
+            {
+                SqlConnection = new SqlConnection(strConn);
+                DataSet = new DataSet();
+            }
+            catch (SystemException e)
+            {
+                System.Windows.Forms.MessageBox.Show(e.Message, "ERROR");
+                return;
+               
+            }
+        
+        
+        }
+        #endregion
+
+        #region Populate the DataSet
+        // Filling the Dataset with fresh table from a specific query
+        protected void FillDataSet(string aSQLstring, string aTable)
+        {
+            try
+            {
+                DataAdapter = new SqlDataAdapter(aSQLstring, aTable);
+                SqlConnection.Open();
+                DataAdapter.Fill(DataSet, aTable);
+                SqlConnection.Close();
+            }
+            catch (Exception errObj)
+            {
+
+                MessageBox.Show(errObj.Message + " " +  errObj.StackTrace);
+            }
+        }
+        #endregion
+
+        #region Update the Data Source
+        protected bool UpdateDataSource(string sqlLocal, string sqlTable) {
+            bool success;
+            try
+            {
+                SqlConnection.Open();
+                DataAdapter.Update(DataSet, sqlTable);
+                SqlConnection.Close();
+                FillDataSet(sqlLocal, sqlTable);
+                success = true;
+            }
+            catch (Exception errObj)
+            {
+                MessageBox.Show(errObj.Message + " " + errObj.StackTrace);
+                success = false;
+            }
+            finally { }
+            return success;
+        }
+        #endregion
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,20 @@ namespace Phumla_System
         {
             InitializeComponent();
         }
+
+        private void customerBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.customerBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.bookingsDatabaseDataSet);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'bookingsDatabaseDataSet.Customer' table. You can move, or remove it, as needed.
+            this.customerTableAdapter.Fill(this.bookingsDatabaseDataSet.Customer);
+
+        }
     }
 }
