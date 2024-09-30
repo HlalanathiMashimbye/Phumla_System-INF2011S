2f17ecd76922feb8e9fdabfd3f28e76da0490657
Lesedi Dickson <lesedi.dicksonxx@gmail.com>
2024-09-30 20:25:40 +0200

    editted fonts in CancelBooking form

--- CancelBooking.Designer.cs
+++ CancelBooking.Designer.cs
@@ -27,66 +27,79 @@ namespace Phumla_System
             // dataGridViewBookings
             // 
             this.dataGridViewBookings.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridViewBookings.Location = new System.Drawing.Point(12, 12);
+            this.dataGridViewBookings.Location = new System.Drawing.Point(18, 18);
+            this.dataGridViewBookings.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.dataGridViewBookings.Name = "dataGridViewBookings";
-            this.dataGridViewBookings.Size = new System.Drawing.Size(776, 300);
+            this.dataGridViewBookings.RowHeadersWidth = 62;
+            this.dataGridViewBookings.Size = new System.Drawing.Size(1164, 462);
             this.dataGridViewBookings.TabIndex = 0;
             this.dataGridViewBookings.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewBookings_CellContentClick);
             // 
             // btnCancelBooking
             // 
-            this.btnCancelBooking.Location = new System.Drawing.Point(12, 380);
+            this.btnCancelBooking.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.btnCancelBooking.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelBooking.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.btnCancelBooking.Location = new System.Drawing.Point(18, 585);
+            this.btnCancelBooking.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnCancelBooking.Name = "btnCancelBooking";
-            this.btnCancelBooking.Size = new System.Drawing.Size(776, 23);
+            this.btnCancelBooking.Size = new System.Drawing.Size(1164, 43);
             this.btnCancelBooking.TabIndex = 3;
             this.btnCancelBooking.Text = "Cancel Booking";
-            this.btnCancelBooking.UseVisualStyleBackColor = true;
+            this.btnCancelBooking.UseVisualStyleBackColor = false;
             this.btnCancelBooking.Click += new System.EventHandler(this.btnCancelBooking_Click);
             // 
             // txtPassword
             // 
-            this.txtPassword.Location = new System.Drawing.Point(74, 347);
+            this.txtPassword.Location = new System.Drawing.Point(152, 538);
+            this.txtPassword.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtPassword.Name = "txtPassword";
             this.txtPassword.PasswordChar = '*';
-            this.txtPassword.Size = new System.Drawing.Size(200, 20);
+            this.txtPassword.Size = new System.Drawing.Size(298, 26);
             this.txtPassword.TabIndex = 2;
             // 
             // txtEmail
             // 
-            this.txtEmail.Location = new System.Drawing.Point(74, 317);
+            this.txtEmail.Location = new System.Drawing.Point(152, 496);
+            this.txtEmail.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtEmail.Name = "txtEmail";
-            this.txtEmail.Size = new System.Drawing.Size(200, 20);
+            this.txtEmail.Size = new System.Drawing.Size(298, 26);
             this.txtEmail.TabIndex = 5;
             // 
             // lblPassword
             // 
             this.lblPassword.AutoSize = true;
-            this.lblPassword.Location = new System.Drawing.Point(12, 350);
+            this.lblPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPassword.Location = new System.Drawing.Point(18, 538);
+            this.lblPassword.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblPassword.Name = "lblPassword";
-            this.lblPassword.Size = new System.Drawing.Size(56, 13);
+            this.lblPassword.Size = new System.Drawing.Size(126, 29);
             this.lblPassword.TabIndex = 1;
             this.lblPassword.Text = "Password:";
             // 
             // lblEmail
             // 
             this.lblEmail.AutoSize = true;
-            this.lblEmail.Location = new System.Drawing.Point(12, 320);
+            this.lblEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmail.Location = new System.Drawing.Point(18, 492);
+            this.lblEmail.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblEmail.Name = "lblEmail";
-            this.lblEmail.Size = new System.Drawing.Size(35, 13);
+            this.lblEmail.Size = new System.Drawing.Size(80, 29);
             this.lblEmail.TabIndex = 4;
             this.lblEmail.Text = "Email:";
             // 
             // CancelBooking
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1200, 692);
             this.Controls.Add(this.lblEmail);
             this.Controls.Add(this.txtEmail);
             this.Controls.Add(this.lblPassword);
             this.Controls.Add(this.txtPassword);
             this.Controls.Add(this.btnCancelBooking);
             this.Controls.Add(this.dataGridViewBookings);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "CancelBooking";
             this.Text = "Cancel Booking";
             this.Load += new System.EventHandler(this.CancelBooking_Load);
